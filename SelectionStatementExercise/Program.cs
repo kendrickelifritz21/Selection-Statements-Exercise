a72e75fd1fdee0cd201f677ebece648c4c26f2dd
Elifritz <Kendrick_Elifritz@Dell.com>
2024-05-08 17:37:47 -0700

    Completed conditionals exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 20);
+
+            Console.WriteLine("Choose a number between 1 and 20");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high");
+            }
+            else
+            {
+                Console.WriteLine("Congrats, you guessed right!");
+            }
         }
     }
 }
